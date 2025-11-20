83cf90997399d65769581adb8d7bb664b0730057
skinny-ke <maishb800@gmail.com>
2025-11-20 23:54:05 +0300

    deleted unnecessary files

--- app/admin/analytics/page.tsx
+++
@@ -1,7 +0,0 @@
-/* filepath: /home/skinny-ke/Desktop/BEN-MARKET/app/admin/analytics/page.tsx */
-import dynamic from "next/dynamic";
-const AdminAnalyticsClient = dynamic(() => import("../../../components/admin/AdminAnalyticsClient"), { ssr: false });
-
-export default function Page() {
-  return <AdminAnalyticsClient />;
-}
